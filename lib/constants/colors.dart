6fb3435c1eef37066bf967456860caa425b5fbba
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-05-27 16:49:52 +0430

    add some colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class IColors {
+
+  static final Color purpleCrimson = Color(0xff934156);
+  static final Color brown = Color(0xffCA735E);
+  static final Color lightBrown = Color(0xffF3EDE7);
+
+}
