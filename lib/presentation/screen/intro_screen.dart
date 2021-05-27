7c95abdc0605c6d4de6015a0c8805c85acdfa7f7
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-05-27 16:45:53 +0430

    create initial project stuff

---
+++ lib/presentation/screen/intro_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class IntroScreen extends StatefulWidget {
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Simple"),
+      
+    );
+  }
+}
