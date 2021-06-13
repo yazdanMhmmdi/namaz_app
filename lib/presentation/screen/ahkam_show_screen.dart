0c67c1dbe2f95a6c2ea2a561e779ab878d771892
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-06-13 18:34:59 +0430

    add border radiuse listener to ahkam show

--- lib/presentation/screen/ahkam_show_screen.dart
+++ lib/presentation/screen/ahkam_show_screen.dart
@@ -19,10 +19,14 @@ class _AhkamShowScreenState extends State<AhkamShowScreen> {
   Map<String, String> arguments;
   String ahkam_id;
   AhkamDetailsBloc _ahkamDetailsBloc;
+  final maxBorderRadius = 50.0;
+  var borderRadius;
   @override
   void initState() {
     arguments = widget.args;
     _getArguments();
+    borderRadius = maxBorderRadius;
+
     _ahkamDetailsBloc = BlocProvider.of<AhkamDetailsBloc>(context);
     _ahkamDetailsBloc.add(GetAhkamDetails(ahkam_id: ahkam_id));
     super.initState();
@@ -42,77 +46,89 @@ class _AhkamShowScreenState extends State<AhkamShowScreen> {
               } else if (state is AhkamDetailsSuccess) {
                 return Stack(
                   children: [
-                    DraggableScrollableSheet(
-                        initialChildSize: 0.7,
-                        minChildSize: 0.7,
-                        builder: (context, scroll) {
-                          return ClipRRect(
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(40),
-                                topRight: Radius.circular(40)),
-                            child: Container(
-                              color: Colors.white,
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.only(top: 30, bottom: 16),
-                                child: Directionality(
-                                  textDirection: TextDirection.rtl,
-                                  child: ListView(
-                                    controller: scroll,
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 16),
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Stack(
-                                            children: [
-                                              Container(
-                                                width: 35,
-                                                height: 35,
-                                                child: Center(
-                                                  child: Text(
-                                                      '${state.ahkamDetailsModel.data.id}',
-                                                      style: TextStyle(
-                                                          fontSize: 14,
-                                                          color: IColors.brown,
-                                                          fontWeight:
-                                                              FontWeight.w700)),
+                    NotificationListener<DraggableScrollableNotification>(
+                      onNotification: (notification) {
+                        borderRadius = notification.maxExtent <=
+                                (notification.extent + 0.08)
+                            ? 0.0
+                            : maxBorderRadius;
+                      },
+                      child: DraggableScrollableSheet(
+                          initialChildSize: 0.7,
+                          minChildSize: 0.7,
+                          builder: (context, scroll) {
+                            return ClipRRect(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(
+                                      double.parse(borderRadius.toString())),
+                                  topRight: Radius.circular(
+                                      double.parse(borderRadius.toString()))),
+                              child: Container(
+                                color: Colors.white,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 30, bottom: 16),
+                                  child: Directionality(
+                                    textDirection: TextDirection.rtl,
+                                    child: ListView(
+                                      controller: scroll,
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 16),
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Stack(
+                                              children: [
+                                                Container(
+                                                  width: 35,
+                                                  height: 35,
+                                                  child: Center(
+                                                    child: Text(
+                                                        '${state.ahkamDetailsModel.data.id}',
+                                                        style: TextStyle(
+                                                            fontSize: 14,
+                                                            color:
+                                                                IColors.brown,
+                                                            fontWeight:
+                                                                FontWeight
+                                                                    .w700)),
+                                                  ),
                                                 ),
-                                              ),
-                                              Image.asset(Assets.largeShamse),
-                                            ],
-                                          ),
-                                          SizedBox(
-                                            width: 8,
-                                          ),
-                                          Flexible(
-                                            child: Text(
-                                              "${state.ahkamDetailsModel.data.title}",
-                                              maxLines: 1,
-                                              overflow: TextOverflow.ellipsis,
-                                              style: TextStyle(
-                                                  fontSize: 14,
-                                                  fontWeight: FontWeight.bold,
-                                                  color: IColors.black70),
+                                                Image.asset(Assets.largeShamse),
+                                              ],
                                             ),
-                                          )
-                                        ],
-                                      ),
-                                      Text(
-                                        "${state.ahkamDetailsModel.data.titleText}",
-                                        maxLines: null,
-                                        style: TextStyle(
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.bold,
-                                            color: IColors.black70),
-                                      )
-                                    ],
+                                            SizedBox(
+                                              width: 8,
+                                            ),
+                                            Flexible(
+                                              child: Text(
+                                                "${state.ahkamDetailsModel.data.title}",
+                                                maxLines: 1,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: TextStyle(
+                                                    fontSize: 14,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: IColors.black70),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                        Text(
+                                          "${state.ahkamDetailsModel.data.titleText}",
+                                          maxLines: null,
+                                          style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.bold,
+                                              color: IColors.black70),
+                                        )
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          );
-                        }),
+                            );
+                          }),
+                    ),
                   ],
                 );
               } else if (state is AhkamDetailsFailure) {
